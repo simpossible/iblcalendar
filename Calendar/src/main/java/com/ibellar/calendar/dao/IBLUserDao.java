5ad0fc71dce2967c7f5390c93cd611955591500a
simpossible <ljfsteven@163.com>
2016-06-24 11:01:38 +0800

    ibluserdao

--- Calendar/src/main/java/com/ibellar/calendar/dao/IBLUserDao.java
+++ Calendar/src/main/java/com/ibellar/calendar/dao/IBLUserDao.java
@@ -1,5 +1,8 @@
 package com.ibellar.calendar.dao;
 
+import java.util.List;
+
+import org.hibernate.Query;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -14,5 +17,20 @@ public class IBLUserDao {
 	public void saveUser(IBLUser u) {
 		sessionFactory.getCurrentSession().save(u);
 		System.out.println(sessionFactory+"121212");
+	}
+	
+	public void deleteUser(IBLUser u) {
+		sessionFactory.getCurrentSession().delete(u);
+	}
+	
+	public IBLUser quearyUserWithEmail(String email) {
+		String hql = "from IBLUser as IBLUser where user.email=:name";
+		Query query = sessionFactory.getCurrentSession().createQuery(hql);
+		query.setString("name", email);
+		
+		List<IBLUser> list = query.list();
+		
+		return list.get(0);
+		
 	}
 }
